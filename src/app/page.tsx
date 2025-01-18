5d1c6218387b8dc410985fb062f90981c0666561
kepengqi <kepengqi@163.com>
2025-01-18 23:44:19 +0800

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image";
+export const runtime = "edge";
 
-export default function Home() {
+
+async function getUsers() {
+    const res = await fetch("https://my-first-worker.anguske2027.workers.dev/");
+  
+  if (!res.ok) {
+    throw new Error('Failed to fetch users');
+  }
+ 
+  return res.json();
+}
+
+export default async function Home() {
+    const user = await getUsers();
+    console.log('user',user);
+    
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
