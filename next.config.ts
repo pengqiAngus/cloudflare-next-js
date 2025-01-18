6ce2b8bc131c043d788e118428ef282601603ed5
kepengqi <kepengqi@163.com>
2025-01-18 23:51:11 +0800

    112

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-//  if (process.env.NODE_ENV === "development") {
-//    await setupDevPlatform();
-//  }
+ if (process.env.NODE_ENV === "development") {
+   await setupDevPlatform();
+ }
 
 export default nextConfig;
